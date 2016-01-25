0e42e8e952f645929622f445bc7085cdb10f9d51
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2016-01-24 17:44:02 -0800

    Changed the processing test stroke width to 3

--- files/posts/processing_test/get_started.js
+++ files/posts/processing_test/get_started.js
@@ -2,7 +2,7 @@ function setup() {
     canvas = createCanvas(windowWidth, 200);
     canvas.parent("get_started")
     background(255);
-    strokeWeight(5);
+    strokeWeight(3);
     stroke(0, 0, 255);
     fill(255)
 }
