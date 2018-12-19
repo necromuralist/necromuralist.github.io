66ff9d5077fb08e75e7575ffd87ab522545f246e
necromuralist <3393973+necromuralist@users.noreply.github.com>
2018-12-19 14:55:19 -0800

    Nikola auto commit.
    Nikola version: 8.0.1

--- conf.py
+++ conf.py
@@ -693,11 +693,12 @@ GITHUB_COMMIT_SOURCE = True
 # <https://getnikola.com/handbook.html#post-processing-filters>
 #
 # from nikola import filters
-# FILTERS = {
-#    ".html": [filters.typogrify],
-#    ".js": [filters.closure_compiler],
-#    ".jpg": ["jpegoptim --strip-all -m75 -v %s"],
-# }
+FILTERS = {
+    ".html": ["filters.html_tidy_mini"],
+#    ".js": ["filters.yui_compressor"],
+#    ".css": ["filter.yui_compressor"],
+    ".png": ["filters.optipng"],
+ }
 
 # Executable for the "yui_compressor" filter (defaults to 'yui-compressor').
 # YUI_COMPRESSOR_EXECUTABLE = 'yui-compressor'
@@ -714,7 +715,7 @@ GITHUB_COMMIT_SOURCE = True
 # Executable for the "html_tidy_withconfig", "html_tidy_nowrap",
 # "html_tidy_wrap", "html_tidy_wrap_attr" and "html_tidy_mini" filters
 # (defaults to 'tidy5').
-# HTML_TIDY_EXECUTABLE = 'tidy5'
+HTML_TIDY_EXECUTABLE = 'tidy'
 
 # List of XPath expressions which should be used for finding headers
 # ({hx} is replaced by headers h1 through h6).
