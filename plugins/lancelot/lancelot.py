82f78cc0dd21a20bad4ccee94ea861e9de6157b5
necromuralist <necromuralist@pm.me>
2020-07-29 13:46:55 -0700

    Nikola auto commit.
    Nikola version: 8.1.1

--- plugins/lancelot/lancelot.py
+++ plugins/lancelot/lancelot.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # This file is public domain according to its author, the Cloisted Monkey
 
-"""Shortcode for non-restructured text inter-site links. 
+"""Shortcode for non-restructured text inter-site links.
 Re-write of the ``doc`` plugin to allow alternative titles outside of RST"""
 
 from nikola.plugin_categories import ShortcodePlugin
@@ -13,28 +13,28 @@ class Plugin(ShortcodePlugin):
 
     name = "lancelot"
 
-    def handler(self, site=None, data=None, lang=None,
-                title=None):
+    def handler(self, title=None, site=None, data=None, lang=None):
         """Create an inter-site link
-    
+
         Args:
          title: optional argument to specify a different title from the post
-    
+
         Returns:
          output HTML to replace the shortcode
         """
-        success, twin_slugs, title, permalink, slug = lancelot_link(site, data, title)
+        success, twin_slugs, title, permalink, slug = lancelot_link(
+            site, data, title)
         if success:
             if twin_slugs:
                 LOGGER.warning(
-                    f'More than one post with the same slug. Using "{permalink}" '
-                    'for lancelot shortcode')
+                    'More than one post with the same slug. '
+                    f'Using "{permalink}" for lancelot shortcode')
             output = f'<a href="{permalink}">{title}</a>'
         else:
             LOGGER.error(
                 f'"{slug}" slug doesn\'t exist.')
             output = ('<span class="error text-error" style="color: red;">'
-                    f'Invalid link: {data}</span>')
+                      f'Invalid link: {data}</span>')
         return output, []
 
 
@@ -57,7 +57,7 @@ def lancelot_link(site, slug, title):
     else:
         fragment = None
     slug = slugify(slug)
-    
+
     twin_slugs = False
     post = None
     for p in site.timeline:
@@ -66,18 +66,16 @@ def lancelot_link(site, slug, title):
                 post = p
             else:
                 twin_slugs = True
-                break        
-    try:
-        if post is None:
-            raise ValueError("No post with matching slug found.")
-    except ValueError:
+                break
+
+    if post is None:
         return False, False, title, None, slug
-    
+
     if title is None:
         title = post.title()
-    
+
     permalink = post.permalink()
     if fragment:
         permalink += '#' + fragment
-    
+
     return True, twin_slugs, title, permalink, slug
