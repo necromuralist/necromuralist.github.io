6a04065df3478d679344bd5d80ba95782eab6082
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2021-11-07 17:09:22 -0800

    replacing email with github noreply

--- conf.py
+++ conf.py
@@ -24,7 +24,7 @@ SITE_URL = "https://necromuralist.github.io/"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://necromuralist.github.io/"
-BLOG_EMAIL = "necromuralist@protonmail.com"
+BLOG_EMAIL = "cloisteredmonkey.jmark@slmail.me"
 BLOG_DESCRIPTION = "A common place."  # (translatable)
 
 # Nikola is multilingual!
