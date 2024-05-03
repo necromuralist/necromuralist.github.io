96fab4ca302df3d86727d4aab02ff8fd226b9425
Cloistered Monkey <3393973+necromuralist@users.noreply.github.com>
2024-05-03 01:13:35 +0100

    Updated conf.py to use different main/pages branch names

--- conf.py
+++ conf.py
@@ -650,8 +650,8 @@ REDIRECTIONS = []
 # For more details, read the manual:
 # https://getnikola.com/handbook.html#deploying-to-github
 # You will need to configure the deployment branch on GitHub.
-GITHUB_SOURCE_BRANCH = 'source'
-GITHUB_DEPLOY_BRANCH = 'main'
+GITHUB_SOURCE_BRANCH = 'main'
+GITHUB_DEPLOY_BRANCH = 'pages'
 
 # The name of the remote where you wish to push to, using github_deploy.
 GITHUB_REMOTE_NAME = 'origin'
